4536da1441de48ecd313e4d3297ad1eae71145d4
osbert <osbert@Osbert-MacBook-Pro.local>
2017-04-24 16:00:11 +0800

    adding 2 types

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 import "fmt"
+
+type bankNoteDeck struct {
+	faceValue float32
+	quantity int
+}
+
+type bankNoteProblem struct {
+	robberShare []int
+	bankNoteDecks []bankNoteDeck
+}
+
 func main() {
     fmt.Println("hello world")
 }
