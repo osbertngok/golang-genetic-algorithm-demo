174e8c8186ebaf3ec33e4cac37f2a048b7a8a330
osbert <osbert@implustech.com>
2017-04-24 18:38:53 +0800

    negative test case

--- main_test.go
+++ main_test.go
@@ -25,3 +25,25 @@ func Test_validateBankNoteProblem_success(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func Test_validateBankNoteProblem_failed(t *testing.T) {
+	var bnp BankNoteProblem
+	robberShareArray := [...]int{
+		10,
+		20,
+		31}
+	bnp.robberShare = robberShareArray[:]
+	bankNoteDecksArray := [...]BankNoteDeck{
+		BankNoteDeck{1, 5},
+		BankNoteDeck{5, 10},
+		BankNoteDeck{10, 10},
+		BankNoteDeck{20, 20},
+		BankNoteDeck{50, 5},
+		BankNoteDeck{100, 10}}
+	bnp.bankNoteDecks = bankNoteDecksArray[:]
+	err := validateBankNoteProblem(&bnp)
+	if err == nil {
+		t.Log("Expected to be error!")
+		t.Fail()
+	}
+}
