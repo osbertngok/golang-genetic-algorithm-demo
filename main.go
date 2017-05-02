353c787ff6e3159ad88315f6bed7495f8b933159
osbert <osbert@implustech.com>
2017-05-02 12:25:22 +0800

    functional main file

--- main.go
+++ main.go
@@ -1,12 +1,49 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	var bnp BankNoteProblem
-	input := os.Stdin
+	input, err := os.Open("./data/bnp1.txt")
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(-1)
+	}
 	readRobberShare(input, &bnp)
 	readBankNoteDecks(input, &bnp)
 	_ = bnp.validate()
-	// output
+	config := GeneticAlgorithmConfig{
+		maxGenerationCount:         20,
+		maxCandidateCount:          100,
+		noOfMutantForEachCandidate: 100,
+		maxMutateCount:             10,
+		maxAttemptCount:            100,
+		intensityFunc: func(generationCount, maxGenerationCount int) float64 {
+			return 1.0
+			/*
+				intensity := 1.0 - float64(generationCount) / float64(maxGenerationCount)
+				if intensity < 0.1 {
+					intensity = 0.1
+				}
+				return intensity
+			*/
+		}}
+	bns, err := bnp.getGeneticAlgorithmSolution(config)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(-1)
+	}
+
+	if err = bns.validate(&bnp); err != nil {
+		fmt.Print(err)
+		os.Exit(-1)
+	}
+	fmt.Println(bns)
+	if fmt.Sprint(bns) != fmt.Sprint(testsbns[2]) {
+		fmt.Print(err)
+		os.Exit(-1)
+	}
 }
